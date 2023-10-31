40b49d6c2ad47bf3cbd3739ccf851bf021934af1
Yael Hernández <111250796+yaelhddz@users.noreply.github.com>
2023-10-31 20:59:48 +0000

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,13 @@ const INSTAGRAM = "https://instagram.com/yaelhddz";
 const DISCORD_ID = "207204046115831809";
 
 const WORK = [
-
+  {
+    company: "Telefónica Spain.",
+    position: "UX Researcher.",
+    link: "https://www.telefonica.com/es/",
+    description:
+      "Telefónica is a Spanish multinational telecommunications company. It is one of the largest telephone operators and mobile network providers in the world.",
+  },
 ];
 
 export { EMAIL, TWITTER, GITHUB, LINKEDIN, INSTAGRAM, DISCORD_ID, WORK };
