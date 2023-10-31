81891ab561abc62d18c3643ac6f742b43f148410
Yael Hernández <111250796+yaelhddz@users.noreply.github.com>
2023-10-31 20:46:00 +0000

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import { styled } from "../../stitches.config";
+import {styled} from "../../stitches.config";
 import AnimatedPage from "../components/AnimatedPage";
 import AnimatedText from "../components/AnimatedText";
-import { TitleWrapper } from "../components/CommonPageStyles";
+import {TitleWrapper} from "../components/CommonPageStyles";
 
 export default function About() {
     return (
@@ -16,20 +16,20 @@ export default function About() {
         interfaces for both consumer and business ends. i love football,
         cats, and design cool stuff! i'm currently working on telefonica."
                 />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
 
             <TitleWrapper>
-                <AnimatedText element="h1" text="About" />
+                <AnimatedText element="h1" text="About"/>
             </TitleWrapper>
             <p>
                 hey, i'm yael, an 18 year old software engineer and designer based in
                 spain. i strive to build unique and polished web
                 interfaces for both consumer and business ends. i love football,
                 cats, and design cool stuff!
-                <br />
-                <br />
+                <br/>
+                <br/>
                 currently working on{" "}
                 <a href="https://www.telefonica.es/es/" target="_blank" rel="noreferrer">
                     Hop
