61ed539fbe96449b0b5601fdd26d3ebd6b10fe0a
Yael Hernández <111250796+yaelhddz@users.noreply.github.com>
2023-11-01 23:25:08 +0000

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,7 @@ import Nav from "../components/Nav";
 import { loadCursor } from "../utils/cursor";
 import { Analytics } from "@vercel/analytics/react";
 import usePreferredTheme from "../utils/hooks/usePreferredTheme";
-import { GeistSans } from "geist/font";
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-      <main className={GeistSans.className}>
-        <Component {...pageProps} />
-      </main>
-  )
-}
 
 const svgAnimation = keyframes({
   "0%": {
